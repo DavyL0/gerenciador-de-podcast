3106e7bb4336f722afe159ee4692699bf952d991
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2025-01-16 18:07:11 -0300

    base do projeto pronta
    -servidor levantado

---
+++ src/controllers/podcasts-controller.ts
@@ -0,0 +1,13 @@
+import {IncomingMessage, request, ServerResponse} from 'http';
+import {serviceListEpisodes } from '../services/list-episodes-services';
+
+export const  getListEpisodes = async (
+    req: IncomingMessage, 
+    res: ServerResponse
+) => {
+    const content = await serviceListEpisodes();
+
+    res.writeHead(200, {'Content-Type': "application/json" });
+    res.end(
+        JSON.stringify(content));
+}
---
+++ src/services/list-episodes-services.ts
@@ -0,0 +1,18 @@
+export const serviceListEpisodes = async()=> {
+    const data = [{
+        podcastName: "flow",
+        episode: "CBUM - Flow #319",
+        videoId: "pQSuQmUfS30",
+        category: ["saude", "bodybuilder"]
+    
+    },
+    {
+        podcastName: "flow",
+        episode: "RUBENS BARRICHELLO - Flow #339",
+        videoId: "4KDGTdiOV4I",
+        category: ["esporte", "corrida"]
+    
+    }
+    ];
+    return data;
+}
