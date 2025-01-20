6b5cf9f887a7b0fa62e2263543d8110c9ba7e56a
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2025-01-20 15:09:37 -0300

    update

--- src/repositories/podcasts-repository.ts
+++ src/repositories/podcasts-repository.ts
@@ -8,6 +8,11 @@ const pathData = path.join(__dirname, "../repositories/podcasts.json");
 //recebe de modo opcional a string
 export const repositoryPodcast = async (podcastName?:string ): Promise<Podcast[]> => {
     const rawData = fs.readFileSync(pathData, "utf-8");
-    const jsonFile = JSON.parse(rawData);
+    let jsonFile = JSON.parse(rawData);
+
+
+    if(podcastName){
+        jsonFile = jsonFile.filter((podcast) => podcast.podcastName === podcastName)
+    }
     return jsonFile;
 }
